a3523ac5de1e615dd85a9fc071e804956b21f7b1
Aditya D <adityadoranala2@gmail.com>
2022-06-09 22:58:53 +0530

    Image features

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
-name = 'cat'
-if name:
-    st.title(f'Hello {name}')
-    img_file_buffer = st.camera_input("Take a picture")
+from streamlit_cropper import st_cropper
+from PIL import Image
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer:
+#     bytes_data = img_file_buffer.getvalue()
+#     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    'Photo taken'
 
     if img_file_buffer:
-        st.image(cv2_img)
-        'Photo taken'
+        img = Image.open(img_file_buffer)
+        cropped_img = st_cropper(img)
+    st.image(cropped_img, width = 1000)
