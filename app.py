1c3cef4baa5b951f0ee7d922e560f6c140f2a19f
Aditya D <adityadoranala2@gmail.com>
2022-06-12 10:34:19 +0530

    Final_app

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 import streamlit as st
-import cv2
-import dlib
-import numpy as np
-# initialisation()
-img_file_buffer = st.camera_input("Take a picture")
+
+@st.experimental_singleton
+def initial():
+    import cv2
+    import dlib
+    import numpy as np
+    import pickle
+    return cv2, dlib, np, pickle
+cv2, dlib, np, pickle = initial()
+st.subheader('Welcome to Ikshana')
+st.image('I5.png')
+###
+with st.expander('Additional Information'):
+#     st.text('Instructions for usage')
+    st.text('1.Please take an image that covers your entire face')
+    st.text('2.Take the picture in good lighting setup')
+    st.text('3.Avoid using flash while taking the picture')
+###
+st.selectbox(label = 'Photo Options', options = ['Camera', 'Upload'], key = 'upload')
+if st.session_state['upload'] == 'Upload':
+    img_file_buffer = st.file_uploader('Upload a picture')
+else:
+    img_file_buffer = st.camera_input('Take a picture')
 labels = ['normal', 'mild', 'severe']
 if img_file_buffer:
     bytes_data = img_file_buffer.getvalue()
     img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    'Photo taken'
+#     'Photo taken'
     @st.experimental_singleton
     def dlib_objs():
         fd = dlib.get_frontal_face_detector()
@@ -18,8 +36,7 @@ if img_file_buffer:
     gray, img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     faces = fd(img)
     if faces:
-        st.balloons()
-        st.success('Its Working')
+        st.success('Face Detected.')
         points = sp(gray, faces[0])
         cx, cy = [i.x for i in points.parts()[36:42]], [i.y for i in points.parts()[36:42]]
         x, y, radius = np.mean(cx, dtype = int), np.mean(cy, dtype = int), (max(cx) - min(cx)) // 2
@@ -31,24 +48,24 @@ if img_file_buffer:
         mask = np.zeros(img.shape, dtype="uint8")
         mask = cv2.circle(mask, center = (x, y), radius = radius, thickness = -1, color = (1, 1, 1))
         left_eye = (img * mask)[y - radius:y + radius, x - radius:x + radius]
-#         st.image((cv2.cvtColor(img, cv2.COLOR_BGR2RGB) * mask)[y - radius:y + radius, x - radius:x + radius], width = 500)
         col1, col2 = st.columns(2)
         col1.header('Left Eye')
-        col1.image(left_eye, width = 300)
+        col1.image(left_eye, width = 200)
         col2.header('Right Eye')
-        col2.image(right_eye, width = 300)
+        col2.image(right_eye, width = 200)
         from tensorflow.keras.models import model_from_json
-        with open('model.json', 'r') as f:
+        with open('vgg.json', 'r') as f:
             js = f.read()
         model = model_from_json(js)
-        model.load_weights('weights.h5')
-        preds = model.predict(np.array([cv2.resize(left_eye, (224, 224)), cv2.resize(right_eye, (224, 224))]))
-        col1.write('Result - ' + labels[np.argmax(preds[0])])
-        col2.write('Result - ' + labels[np.argmax(preds[1])])
+        model.load_weights('vgg_weights.h5')
+        with open('xgb.pkl', 'rb') as f:
+            xg = pickle.load(f)
+#         preds = model.predict(np.array([cv2.resize(left_eye, (224, 224)), cv2.resize(right_eye, (224, 224))]))
+        preds = xg.predict(model.predict(np.array([cv2.resize(left_eye, (224, 224)), cv2.resize(right_eye, (224, 224))])))
+        label = ['No Cataract Detected.', 'Mild Cataract Detected.', 'Severe Cataract Detected.\nReach out to an eye doctor soon.']
+        col1.info(label[np.argmax(preds[0])])
+        col2.info(label[np.argmax(preds[1])])
+        st.warning('Ensure Quality for Accurate Results.')
     else:
-        st.error('Where\'s your face?')
-
-#     if img_file_buffer:
-#         img = Image.open(img_file_buffer)
-#         cropped_img = st_cropper(img)
-#     st.image(cropped_img, width = 1000)
+        st.error('Face Detection Failed. Please Try Again!!!')
+    
